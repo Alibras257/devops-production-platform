70c0544a921c56365e176b30d70bcdc7e8db6657
Alibras257 <Alibras257@gmail.com>
2026-08-04 21:40:15 +0100

    Format backend code with black

--- app/backend/models.py
+++ app/backend/models.py
@@ -9,8 +9,4 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "email": self.email
-        }
+        return {"id": self.id, "name": self.name, "email": self.email}
