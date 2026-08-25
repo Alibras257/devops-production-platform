21bf40541e5fa91dda3537c122df79cd839b5347
Alibras257 <Alibras257@gmail.com>
2026-08-25 23:21:18 +0100

    Update deployment script to run Flask-Migrate upgrades

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,6 +1,6 @@
 import os
 
-from extensions import db
+from extensions import db, migrate
 from flask import Flask, jsonify, request
 from models import User
 from prometheus_flask_exporter import PrometheusMetrics
@@ -29,6 +29,7 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+    migrate.init_app(app, db)
     PrometheusMetrics(app)
 
     @app.route("/")
